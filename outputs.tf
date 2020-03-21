1872c3d2e9fc811d046a5f6ee4d4ebb840bdf57a
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-21 16:41:47 +0000

    added outputs for default security group

--- outputs.tf
+++ outputs.tf
@@ -12,32 +12,42 @@ output "vpc_id" {
   description = "The ID of the VPC"
 }
 
-output "vpc_cidr_block" {
+output "cidr_block" {
   value       = aws_vpc.default.cidr_block
   description = "The CIDR block of the VPC"
 }
 
-output "vpc_main_route_table_id" {
+output "security_group_id" {
+  value       = aws_security_group.default.id
+  description = "The ID of the security group"
+}
+
+output "security_group_arn" {
+  value       = aws_security_group.default.arn
+  description = "The ARN of the security group"
+}
+
+output "main_route_table_id" {
   value       = aws_vpc.default.main_route_table_id
   description = "The ID of the main route table associated with this VPC"
 }
 
-output "vpc_default_network_acl_id" {
+output "default_network_acl_id" {
   value       = aws_vpc.default.default_network_acl_id
   description = "The ID of the network ACL created by default on VPC creation"
 }
 
-output "vpc_default_security_group_id" {
+output "default_security_group_id" {
   value       = aws_vpc.default.default_security_group_id
   description = "The ID of the security group created by default on VPC creation"
 }
 
-output "vpc_default_route_table_id" {
+output "default_route_table_id" {
   value       = aws_vpc.default.default_route_table_id
   description = "The ID of the route table created by default on VPC creation"
 }
 
-output "vpc_ipv6_association_id" {
+output "ipv6_association_id" {
   value       = aws_vpc.default.ipv6_association_id
   description = "The association ID for the IPv6 CIDR block"
 }
